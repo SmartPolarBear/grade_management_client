55cc7920893ed973e3538d3560595a9b0992035d
SmartPolarBear <clevercoolbear@outlook.com>
2023-06-17 21:25:55 +0800

    feat(data): update database

---
+++ GradeManagement/Data/Model/Stc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradeManagement.Data.Model;
+
+[PrimaryKey("TeacherId", "StudentId")]
+[Table("STC")]
+public partial class Stc
+{
+    [Key]
+    [Column("StudentID")]
+    [StringLength(14)]
+    [Unicode(false)]
+    public string StudentId { get; set; } = null!;
+
+    [Key]
+    [Column("TeacherID")]
+    [StringLength(14)]
+    [Unicode(false)]
+    public string TeacherId { get; set; } = null!;
+
+    [Column("CourseID")]
+    [StringLength(14)]
+    [Unicode(false)]
+    public string CourseId { get; set; } = null!;
+
+    public byte Rating { get; set; }
+
+    [ForeignKey("CourseId")]
+    [InverseProperty("Stcs")]
+    public virtual Course Course { get; set; } = null!;
+
+    [ForeignKey("StudentId")]
+    [InverseProperty("Stcs")]
+    public virtual Student Student { get; set; } = null!;
+
+    [ForeignKey("TeacherId")]
+    [InverseProperty("Stcs")]
+    public virtual Teacher Teacher { get; set; } = null!;
+}
