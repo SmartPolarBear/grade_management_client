49dcf7254452a600827235342cb64419bd51d870
SmartPolarBear <clevercoolbear@outlook.com>
2023-07-04 23:56:24 +0800

    feat(admin): finish add student

--- GradeManagement/View/Admin/AddStudentDialog.xaml.cs
+++ GradeManagement/View/Admin/AddStudentDialog.xaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using GradeManagement.Data;
 using GradeManagement.Utils.Validation;
 
 namespace GradeManagement.View.Admin;
@@ -14,16 +18,39 @@ public partial class AddStudentDialog : Window
 
     private void NumericIdValidationRule_OnValidateId(object? sender, ValidateIdEventArgs e)
     {
-        throw new System.NotImplementedException();
+        var dbc = new GradeManagementContext(UserType.Admin);
+        if (e.Id != null && dbc.Students.Any(i => i.Id == e.Id))
+        {
+            e.IsValid = false;
+        }
     }
 
     private void OkButton_OnClick(object sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        if (string.IsNullOrEmpty(Student.Id) || string.IsNullOrEmpty(Student.Name) ||
+            string.IsNullOrEmpty(Student.Email))
+        {
+            MessageBox.Show("Please fill all the fields", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+
+        this.DialogResult = true;
+        this.Close();
     }
 
     private void CancelButton_OnClick(object sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        this.DialogResult = false;
+        this.Close();
+    }
+
+    private void GenderComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Student.Gender = GenderComboBox.SelectedIndex switch
+        {
+            0 => (int)Gender.Female,
+            1 => (int)Gender.Male,
+            _ => 0
+        };
     }
 }
